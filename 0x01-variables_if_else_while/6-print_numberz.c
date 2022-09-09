5756c5908e9a091999a54afe8a62b67d6a99d36b
Sammylayo <sammy15layo@gmail.com>
2022-09-09 16:29:38 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - lmxoqmxoqxqpmqpwmpoqmdpoqwmpoqmpoq
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch;
+
+		for (ch = '0'; ch <= '9'; ch++)
+			putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
