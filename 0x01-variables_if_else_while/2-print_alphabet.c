eb20fb86f07a36cf4c70b4725f99e5fe3d0f6ce0
Sammylayo <sammy15layo@gmail.com>
2022-09-09 14:51:57 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - alphabets
+ * Return: 0
+ */
+
+int main()
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+		putchar("\n");
+
+	return (0);
+}
