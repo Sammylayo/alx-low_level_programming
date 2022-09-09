10aaee9b2468f088a15216f2f36f64d3a1cafa91
Sammylayo <sammy15layo@gmail.com>
2022-09-08 19:39:43 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,6 @@
 
 int main(void)
 {
-	int i;
-	char c;
-	double d;
 	printf("SIze of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
