c84b8aee73ec0035ce1e236811f41f1dea4fa5d0
Sammylayo <sammy15layo@gmail.com>
2022-09-09 16:47:10 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - lmxoqmxoqxqpmqpwmpoqmdpoqwmpoqmpoq
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch;
+
+		for (ch = '0'; ch <= '9'; ch++)
+			putchar(ch);
+			putchar(',');
+			putchar(' ');
+
+	putchar('$');
+
+
+	return (0);
+}
